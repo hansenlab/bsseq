f1504c41c3aa44e6e67ec16c98f7fee197199d12
khansen <khansen@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-03-10 17:48:48 +0000

    Commit made by the Bioconductor Git-SVN bridge.
    Commit id: cb084361bf813e05e2279000303be55e8d07107c
    
        modified permutation code
    
    
    Commit id: 083a52bc284f632f8c6e471d136bff88577b6813
    
        permutation code, not exported
    
    
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/bsseq@100482 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/permutations.R
@@ -0,0 +1,104 @@
+getNullPermutation <- function(BSseq, idxMatrix1, idxMatrix2,
+                               estimate.var, local.correct,
+                               cutoff, stat, maxGap, mc.cores = 1) {
+    stopifnot(nrow(idxMatrix1) == nrow(idxMatrix2))
+    nullDist <- mclapply(1:nrow(idxMatrix1), function(ii) {
+        BS.tstat <- BSmooth.tstat(BSseq, estimate.var = estimate.var,
+                                  group1 = idxMatrix1[ii,],
+                                  group2 = idxMatrix2[ii,],
+                                  local.correct = local.correct, maxGap = 10^8)
+        dmrs0 <- dmrFinder(BS.tstat, stat = stat, cutoff = cutoff, maxGap = maxGap)
+        dmrs0
+    }, mc.cores = min(nrow(idxMatrix1), mc.cores), mc.preschedule = FALSE)
+    nullDist
+}
+
+getNullBlocks <- function(BSseq, idxMatrix1, idxMatrix2, estimate.var = "same",
+                          mc.cores = 1) {
+    getNullPermutation(BSseq = BSseq, idxMatrix1 = idxMatrix1,
+                       idxMatrix2 = idxMatrix2, local.correct = FALSE,
+                       estimate.var = estimate.var,
+                       cutoff = c(-2,2), stat = "tstat", maxGap = 10000,
+                       mc.cores = mc.cores)
+}
+
+getNullDmrs <- function(BSseq, idxMatrix1, idxMatrix2, estimate.var = "same",
+                        mc.cores = 1) {
+    getNullPermutation(BSseq = BSseq, idxMatrix1 = idxMatrix1,
+                       idxMatrix2 = idxMatrix2, local.correct = TRUE,
+                       estimate.var = estimate.var,
+                       cutoff = c(-4.6,4.6), stat = "tstat.corrected", maxGap = 300,
+                       mc.cores = mc.cores)
+}
+
+subsetDmrs <- function(xx) {
+    if(is.null(xx) || is(xx, "try-error"))
+        return(NULL)
+    subset(xx, n >= 3 & abs(meanDiff) > 0.1 & invdensity <= 300)
+}
+
+subsetBlocks <- function(xx) {
+    if(is.null(xx) || is(xx, "try-error"))
+        return(NULL)
+    subset(xx, width >= 10000)
+}
+
+getFWER <- function(null, type = "blocks") {
+    reference <- null[[1]]
+    null <- null[-1]
+    better <- sapply(1:nrow(reference), function(ii) {
+        meanDiff <- abs(reference$meanDiff[ii])
+        width <- reference$width[ii]
+        n <- reference$n[ii]
+        if(type == "blocks") {
+            out <- sapply(null, function(nulldist) {
+                any(abs(nulldist$meanDiff) >= meanDiff &
+                        nulldist$width >= width)
+            })
+        }
+        if(type == "dmrs") {
+            out <- sapply(null, function(nulldist) {
+                any(abs(nulldist$meanDiff) >= meanDiff &
+                    nulldist$n >= n)
+            })
+        }
+        sum(out)
+    })
+    better
+}
+       
+makeIdxMatrix <- function(group1, group2) {
+    stopifnot(length(group2) >= length(group1))
+    stopifnot(length(group1) > 0 && length(group1) <= 3)
+    stopifnot(length(group2) > 0 && length(group2) <= 3)
+    groupBoth <- c(group1, group2)
+    if(length(group1) == 1) {
+        idxMatrix1 <- as.matrix(c(group1, group2))
+    }
+    if(length(group1) == 2) {
+        idxMatrix1 <- rbind(group1,
+                           cbind(group1[1], group2),
+                           cbind(group1[2], group2))
+        if(length(group2) == 3)
+            idxMatrix1 <- rbind(idxMatrix1,
+                                group2[c(1,2)],
+                                group2[c(1,3)],
+                                group2[c(2,3)])
+    }
+    if(length(group1) == 3) {
+        idxMatrix1 <- rbind(group1,
+                            c(group1[1:2], group2[1]),
+                            c(group1[1:2], group2[2]),
+                            c(group1[1:2], group2[3]),
+                            c(group1[c(1,3)], group2[1]),
+                            c(group1[c(1,3)], group2[2]),
+                            c(group1[c(1,3)], group2[3]),
+                            c(group1[c(2,3)], group2[1]),
+                            c(group1[c(2,3)], group2[2]),
+                            c(group1[c(2,3)], group2[3]))
+    }
+    idxMatrix2 <- do.call(rbind, lapply(1:nrow(idxMatrix1), function(ii) {
+        setdiff(groupBoth, idxMatrix1[ii,])
+    }))
+    return(list(idxMatrix1 = idxMatrix1, idxMatrix2 = idxMatrix2))
+}
