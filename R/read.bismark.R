20c8b3bb60b0f5d44ae9424eda8c0cb64ad7f4ec
khansen <khansen@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-06-11 14:04:52 +0000

    Commit made by the Bioconductor Git-SVN bridge.
    Commit id: 3e3c4a483adc9ba1e5d101a35f9a0fef41d2b78b
    
        updated vignettes to use BiocStyle
    
    
    Commit id: fe749c1df8a2a39ca6c4e1cc3b2358195f307631
    
        updating read.bismark
    
    
    Commit id: c50eb68c8e7f1be51af4ff411c95064849b52d15
    
        Fixing Description; updating bsseq.Rnw to use BiocStyle
    
    
    Commit id: f38a77394f0248aae1a6a4e27102610a53ca892e
    
        resaved with compress=xz
    
    
    Commit id: 2dcedd4fead08fe0af2a0afb7de2c89f4818b962
    
        added Raw parsing of cytosine report files from Bismark
    
    
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/bsseq@104827 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/read.bismark.R
+++ R/read.bismark.R
@@ -1,4 +1,5 @@
-read.bismark <- function(files, sampleNames, rmZeroCov = FALSE, verbose = TRUE){
+read.bismark <- function(files, sampleNames, rmZeroCov = FALSE, strandCollapse = TRUE,
+                         fileType = c("cytosineReport", "oldBedGraph"), verbose = TRUE){
     ## Argument checking
     if (anyDuplicated(files)){
         stop("duplicate entries in 'files'")
@@ -6,6 +7,7 @@ read.bismark <- function(files, sampleNames, rmZeroCov = FALSE, verbose = TRUE){
     if (length(sampleNames) != length(files) | anyDuplicated(sampleNames)){
         stop("argument 'sampleNames' has to have the same length as argument 'files', without duplicate entries")
     }
+    fileType <- match.arg(fileType)
     ## Process each file
     idxes <- seq_along(files)
     names(idxes) <- sampleNames
@@ -14,12 +16,19 @@ read.bismark <- function(files, sampleNames, rmZeroCov = FALSE, verbose = TRUE){
             cat(sprintf("[read.bismark] Reading file '%s' ... ", files[ii]))
         }
         ptime1 <- proc.time()
-        raw <- read.bismarkFileRaw(thisfile = files[ii])
+        if(fileType == "oldBedGraph") {
+            raw <- read.bismarkFileRaw(thisfile = files[ii])
+        }
+        if(fileType == "cytosineReport") {
+            raw <- read.bismarkCytosineRaw(thisfile = files[ii], keepContext = FALSE)
+        }
         M <- matrix(mcols(raw)[, "mCount"], ncol = 1)
         Cov <- M + mcols(raw)[, "uCount"]
         mcols(raw) <- NULL
         out <- BSseq(gr = raw, M = M, Cov = Cov,
                      sampleNames = sampleNames[ii], rmZeroCov = rmZeroCov)
+        if(strandCollapse && !all(runValue(strand(out)) == "*"))
+            out <- strandCollapse(out)
         ptime2 <- proc.time()
         stime <- (ptime2 - ptime1)[3]
         if (verbose) {
@@ -62,5 +71,28 @@ read.bismarkFileRaw <- function(thisfile, verbose = TRUE){
     out <- out[!sapply(out, is.null)]
     df <- DataFrame(out)
     mcols(gr) <- df
+    gr
+}
+
+read.bismarkCytosineRaw <- function(thisfile, keepContext = FALSE) {
+    out <- fread(thisfile)
+    if(length(out) != 6 && length(out) != 7)
+        stop("unknown file format")
+    if(length(out) == 6) {
+        setnames(out, c("chr", "start", "end", "methPerc", "mCount", "uCount"))
+        gr <- GRanges(seqnames = out[["chr"]],
+                      ranges = IRanges(start = out[["start"]], width = 1),
+                      mCount = out[["mCount"]], uCount = out[["uCount"]])
+    }
+    if(length(out) == 7) {
+        setnames(out, c("chr", "start", "strand", "mCount", "uCount", "type", "context"))
+        gr <- GRanges(seqnames = out[["chr"]], strand = out[["strand"]],
+                      ranges = IRanges(start = out[["start"]], width = 1),
+                      mCount = out[["mCount"]], uCount = out[["uCount"]])
+        if(keepContext) {
+            gr$type <- out[["type"]]
+            gr$context <- out[["context"]]
+        }                      
+    }
     gr
 }
