731e6342b9bff81916d366908bb2ce4d64375610
Peter Hickey <peter.hickey@gmail.com>
2017-07-02 22:19:32 -0400

    Fix performance regression in getMeth() [https://support.bioconductor.org/p/97611/]
    Rather than 'split the overlaps' and then load in chunks of the data [as in the previous implementation of getMeth()], we load the methylation data in the regions into memory and split in memory. This is **much** more efficient but with the cost of loading more data into memory.

--- R/BSseq_utils.R
+++ R/BSseq_utils.R
@@ -55,86 +55,97 @@ orderBSseq <- function(BSseq, seqOrder = NULL) {
 }
 
 
+# TODO: getMeth() realises the result in memory iff regions is not NULL;
+#       discuss with Kasper
+# TODO: Whether or not colnames are added to returned value depends on whether
+#       regions is non-NULL; discuss with Kasper
+# TODO: Add parallel support
 getMeth <- function(BSseq, regions = NULL, type = c("smooth", "raw"),
-                    what = c("perBase", "perRegion"), confint = FALSE, alpha = 0.95) {
+                    what = c("perBase", "perRegion"), confint = FALSE,
+                    alpha = 0.95) {
     p.conf <- function(p, n, alpha) {
         z <- abs(qnorm((1 - alpha)/2, mean = 0, sd = 1))
-        upper <- (p + z^2/(2*n) + z*sqrt(  (p*(1-p) + z^2/(4*n)) / n)) /
-            (1+z^2/n)
-        lower <- (p + z^2/(2*n) - z*sqrt(  (p*(1-p) + z^2/(4*n)) / n)) /
-            (1+z^2/n)
+        upper <- (p + z ^ 2 / (2 * n) +
+                      z * sqrt((p * (1 - p) + z ^ 2 / (4 * n)) / n)) /
+            (1 + z ^ 2 / n)
+        lower <- (p + z ^ 2 / (2 * n) -
+                      z * sqrt((p * (1 - p) + z ^ 2 / (4 * n)) / n)) /
+            (1 + z ^ 2 / n)
         return(list(meth = p, lower = lower, upper = upper))
     }
+
     stopifnot(is(BSseq, "BSseq"))
     type <- match.arg(type)
-    if(type == "smooth" & !hasBeenSmoothed(BSseq))
+    if (type == "smooth" & !hasBeenSmoothed(BSseq)) {
         stop("'type=smooth' requires the object to have been smoothed.")
+    }
     what <- match.arg(what)
+    if (what == "perRegion" & is.null(regions)) {
+        stop("'what=perRegion' but no 'regions' supplied")
+    }
     z <- abs(qnorm((1 - alpha)/2, mean = 0, sd = 1))
-    if(is.null(regions) && type == "smooth") {
-        meth <- getBSseq(BSseq, type = "trans")(getBSseq(BSseq, type = "coef"))
-        if(confint) {
-            upper <- getBSseq(BSseq, type = "trans")(getBSseq(BSseq, type = "coef") +
-                                      z * getBSseq(BSseq, type = "se.coef"))
-            lower <- getBSseq(BSseq, type = "trans")(getBSseq(BSseq, type = "coef") -
-                                      z * getBSseq(BSseq, type = "se.coef"))
+    if (is.null(regions) && type == "smooth") {
+        coef <- getBSseq(BSseq, type = "coef")
+        meth <- getBSseq(BSseq, type = "trans")(coef)
+        if (confint) {
+            upper <- meth + z * getBSseq(BSseq, type = "se.coef")
+            lower <- meth - z * getBSseq(BSseq, type = "se.coef")
             return(list(meth = meth, lower = lower, upper = upper))
         } else {
             return(meth)
         }
     }
-    if(is.null(regions) && type == "raw") {
+    if (is.null(regions) && type == "raw") {
         meth <- getBSseq(BSseq, type = "M") / getBSseq(BSseq, type = "Cov")
-        if(confint) {
-            return(p.conf(meth, n = getBSseq(BSseq, type = "Cov"), alpha = alpha))
+        if (confint) {
+            return(p.conf(meth, n = getBSseq(BSseq, type = "Cov"),
+                          alpha = alpha))
         } else {
             return(meth)
         }
     }
+
     ## At this point, regions have been specified
-    if(class(regions) == "data.frame")
+    if (class(regions) == "data.frame") {
         regions <- data.frame2GRanges(regions)
-    stopifnot(is(regions, "GenomicRanges"))
-    if(confint) stop("'confint = TRUE' is not supported by 'getMeth' when regions is given")
-    grBSseq <- granges(BSseq)
-    mm <- as.matrix(findOverlaps(regions, grBSseq))
-    mmsplit <- split(mm[,2], mm[,1])
-    if(what == "perBase") {
-        if(type == "smooth") {
-            out <- lapply(mmsplit, function(xx) {
-                getBSseq(BSseq, "trans")(getBSseq(BSseq, "coef")[xx,,drop = FALSE])
-            })
-        }
-        if(type == "raw") {
-            out <- lapply(mmsplit, function(xx) {
-                getBSseq(BSseq, "M")[xx,,drop = FALSE] / getBSseq(BSseq, "Cov")[xx,,drop = FALSE]
-            })
-        }
-        outList <- vector("list", length(regions))
-        outList[as.integer(names(mmsplit))] <- out
-        return(outList)
     }
-    if(what == "perRegion") {
-        if(type == "smooth") {
-            out <- lapply(mmsplit, function(xx) {
-                colMeans(getBSseq(BSseq, "trans")(getBSseq(BSseq, "coef")[xx,,drop = FALSE]), na.rm = TRUE)
-            })
-        }
-        if(type == "raw") {
-            out <- lapply(mmsplit, function(xx) {
-                colMeans(getBSseq(BSseq, "M")[xx,,drop = FALSE] / getBSseq(BSseq, "Cov")[xx,,drop = FALSE], na.rm = TRUE)
-            })
-        }
-        out <- do.call(rbind, out)
+    stopifnot(is(regions, "GenomicRanges"))
+    if (confint) {
+        stop("'confint = TRUE' is not supported by 'getMeth' when regions is given")
+    }
+    grBSseq <- granges(BSseq)
+    ov <- findOverlaps(grBSseq, regions)
+    # NOTE: This realises a large object in memory (`meth`) - could do it in
+    #       chunks if what = perRegion
+    if (type == "smooth") {
+        meth <- as.matrix(
+            getBSseq(BSseq, "trans")(getBSseq(BSseq, "coef"))[queryHits(ov), ,
+                                                              drop = FALSE])
+    } else if (type == "raw") {
+        meth <- as.matrix(
+            (getBSseq(BSseq, "M") / getBSseq(BSseq, "Cov"))[queryHits(ov), ,
+                                                            drop = FALSE])
+    }
+    out <- lapply(split(meth, subjectHits(ov)), matrix, ncol = ncol(meth))
+    if (what == "perBase") {
+        # TODO: Don't really understand the logic of the remaining code; how
+        #       could the results end up in the wrong order wrt to regions?
+        outList <- vector("list", length(regions))
+        outList[as.integer(names(out))] <- out
+        return(outList)
+    } else if (what == "perRegion") {
+        out <- do.call(rbind, lapply(out, colMeans2, na.rm = TRUE))
+        # TODO: Don't really understand the logic of the remaining code; how
+        #       could the rows end up in the wrong order?
         outMatrix <- matrix(NA, ncol = ncol(BSseq), nrow = length(regions))
         colnames(outMatrix) <- sampleNames(BSseq)
-        outMatrix[as.integer(rownames(out)),] <- out
+        outMatrix[as.integer(rownames(out)), ] <- out
         .DelayedMatrix(outMatrix)
     }
 }
 
 getCoverage <- function(BSseq, regions = NULL, type = c("Cov", "M"),
-                    what = c("perBase", "perRegionAverage", "perRegionTotal")) {
+                        what = c("perBase", "perRegionAverage", "perRegionTotal")) {
     stopifnot(is(BSseq, "BSseq"))
     type <- match.arg(type)
     what <- match.arg(what)
