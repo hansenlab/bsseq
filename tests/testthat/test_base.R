841ce47f0ab87e8c900378fabd23107886740bda
PeteHaitch <peter.hickey@gmail.com>
2020-04-24 13:23:25 +1000

    Skip Windows-specific failing test

--- tests/testthat/test_base.R
+++ tests/testthat/test_base.R
@@ -59,6 +59,9 @@ test_that("BSseq with HDF5Backend", {
 })
 
 test_overlaps <- function() {
+    # TODO: Re-enable test on Windows once
+    #       https://github.com/Bioconductor/HDF5Array/issues/29 is fixed.
+    skip_on_os("windows")
     M <- matrix(1:9, 3, 3)
     colnames(M) <- c("A1", "A2", "A3")
     Cov <- M + 2
