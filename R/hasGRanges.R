659aee2ab5a3eb073a31ba3a98e7d0aaab2095a6
Hervé Pagès <hpages@fredhutch.org>
2020-05-20 15:23:58 -0700

    Repair subsetByOverlaps() on hasGRanges derivatives
    Also add overlapsAny() methods and improve granges() method for
    hasGRanges derivatives.

--- R/hasGRanges.R
+++ R/hasGRanges.R
@@ -7,9 +7,7 @@ setMethod("seqnames", signature(x = "hasGRanges"), function(x) {
     seqnames(x@gr)
 })
 setReplaceMethod("seqnames", "hasGRanges", function(x, value) {
-    gr <- granges(x)
-    seqnames(gr) <- value
-    x@gr <- gr
+    seqnames(x@gr) <- value
     x
 })
 
@@ -17,9 +15,7 @@ setMethod("seqlevels", signature(x = "hasGRanges"), function(x) {
     seqlevels(x@gr)
 })
 setReplaceMethod("seqlevels", "hasGRanges", function(x, value) {
-    gr <- granges(x)
-    seqlevels(gr) <- value
-    x@gr <- gr
+    seqlevels(x@gr) <- value
     x
 })
 
@@ -27,14 +23,14 @@ setMethod("seqlengths", signature(x = "hasGRanges"), function(x) {
     seqlengths(x@gr)
 })
 setReplaceMethod("seqlengths", "hasGRanges", function(x, value) {
-    gr <- granges(x)
-    seqlengths(gr) <- value
-    x@gr <- gr
+    seqlengths(x@gr) <- value
     x
 })
 
 setMethod("granges", signature(x = "hasGRanges"),
-          function(x) x@gr)
+    function(x, use.names = TRUE, use.mcols = FALSE, ...)
+        granges(x@gr, use.names = use.names, use.mcols = use.mcols, ...)
+)
 
 ## FIXME: might want a granges replacement function
 
@@ -42,9 +38,7 @@ setMethod("start", "hasGRanges", function(x, ...) {
     start(x@gr, ...)
 })
 setReplaceMethod("start", "hasGRanges", function(x, check = TRUE, value) {
-    gr <- granges(x)
-    start(gr, check = check) <- value
-    x@gr <- gr
+    start(x@gr, check = check) <- value
     x
 })
 
@@ -52,9 +46,7 @@ setMethod("end", "hasGRanges", function(x, ...) {
     end(x@gr, ...)
 })
 setReplaceMethod("end", "hasGRanges", function(x, check = TRUE, value) {
-    gr <- granges(x)
-    end(gr, check = check) <- value
-    x@gr <- gr
+    end(x@gr, check = check) <- value
     x
 })
 
@@ -62,9 +54,7 @@ setMethod("width", "hasGRanges", function(x) {
     width(x@gr)
 })
 setReplaceMethod("width", "hasGRanges", function(x, check = TRUE, value) {
-    gr <- granges(x)
-    width(gr, check = check) <- value
-    x@gr <- gr
+    width(x@gr, check = check) <- value
     x
 })
 
@@ -72,49 +62,20 @@ setMethod("strand", "hasGRanges", function(x) {
     strand(x@gr)
 })
 setReplaceMethod("strand", "hasGRanges", function(x, value) {
-    gr <- granges(x)
-    strand(gr) <- value
-    x@gr <- gr
+    strand(x@gr) <- value
     x
 })
 
 setMethod("length", "hasGRanges", function(x) length(x@gr))
 
-## setMethod("subsetByOverlaps",
-##           signature(query = "hasGRanges", subject = "GenomicRanges"),
-##           function(query, subject, maxgap = 0L, minoverlap = 1L,
-##                    type = c("any", "start", "end", "within", "equal"),
-##                    ignore.strand = FALSE, ...) {
-##               ov <- findOverlaps(query = granges(query), subject = subject,
-##                                  maxgap = maxgap, minoverlap = minoverlap,
-##                                  type = match.arg(type), select = "arbitrary",
-##                                  ignore.strand = ignore.strand, ... )
-##               query[!is.na(ov)]
-##           })
-
-## setMethod("subsetByOverlaps",
-##           signature(query = "hasGRanges", subject = "hasGRanges"),
-##           function(query, subject, maxgap = 0L, minoverlap = 1L,
-##                    type = c("any", "start", "end", "within", "equal"),
-##                    ignore.strand = FALSE, ...) {
-##               ov <- findOverlaps(query = granges(query), subject = granges(subject),
-##                                  maxgap = maxgap, minoverlap = minoverlap,
-##                                  type = match.arg(type), select = "arbitrary",
-##                                  ignore.strand = ignore.strand, ... )
-##               query[!is.na(ov)]
-##           })
-
-## setMethod("subsetByOverlaps",
-##           signature(query = "GenomicRanges", subject = "hasGRanges"),
-##           function(query, subject, maxgap = 0L, minoverlap = 1L,
-##                    type = c("any", "start", "end", "within", "equal"),
-##                    ignore.strand = FALSE, ...) {
-##               ov <- findOverlaps(query = query, subject = granges(subject),
-##                                  maxgap = maxgap, minoverlap = minoverlap,
-##                                  type = match.arg(type), select = "arbitrary",
-##                                  ignore.strand = ignore.strand, ... )
-##               query[!is.na(ov)]
-##           })
+setMethod("[", "hasGRanges", function(x, i, ...) {
+    if(missing(i))
+        stop("need [i] for subsetting")
+    if(missing(i))
+        return(x)
+    x@gr <- x@gr[i]
+    x
+})
 
 setMethod("findOverlaps",
           signature(query = "hasGRanges", subject = "GenomicRanges"),
@@ -122,19 +83,7 @@ setMethod("findOverlaps",
                     type = c("any", "start", "end", "within", "equal"),
                     select = c("all", "first", "last", "arbitrary"),
                     ignore.strand = FALSE, ...) {
-              findOverlaps(query = granges(query), subject = subject,
-                           maxgap = maxgap, minoverlap = minoverlap,
-                           type = match.arg(type), select = match.arg(select),
-                           ignore.strand = ignore.strand, ...)
-          })
-
-setMethod("findOverlaps",
-          signature(query = "hasGRanges", subject = "hasGRanges"),
-          function (query, subject, maxgap = -1L, minoverlap = 0L,
-                    type = c("any", "start", "end", "within", "equal"),
-                    select = c("all", "first", "last", "arbitrary"),
-                    ignore.strand = FALSE, ...) {
-              findOverlaps(query = granges(query), subject = granges(subject),
+              findOverlaps(query = query@gr, subject = subject,
                            maxgap = maxgap, minoverlap = minoverlap,
                            type = match.arg(type), select = match.arg(select),
                            ignore.strand = ignore.strand, ...)
@@ -146,18 +95,41 @@ setMethod("findOverlaps",
                     type = c("any", "start", "end", "within", "equal"),
                     select = c("all", "first", "last", "arbitrary"),
                     ignore.strand = FALSE, ...) {
-              findOverlaps(query = query, subject = granges(subject),
+              findOverlaps(query = query, subject = subject@gr,
                            maxgap = maxgap, minoverlap = minoverlap,
                            type = match.arg(type), select = match.arg(select),
                            ignore.strand = ignore.strand, ...)
           })
 
-setMethod("[", "hasGRanges", function(x, i, ...) {
-    if(missing(i))
-        stop("need [i] for subsetting")
-    if(missing(i))
-        return(x)
-    x@gr <- x@gr[i]
-    x
-})
+setMethod("findOverlaps",
+          signature(query = "hasGRanges", subject = "hasGRanges"),
+          function (query, subject, maxgap = -1L, minoverlap = 0L,
+                    type = c("any", "start", "end", "within", "equal"),
+                    select = c("all", "first", "last", "arbitrary"),
+                    ignore.strand = FALSE, ...) {
+              findOverlaps(query = query@gr, subject = subject@gr,
+                           maxgap = maxgap, minoverlap = minoverlap,
+                           type = match.arg(type), select = match.arg(select),
+                           ignore.strand = ignore.strand, ...)
+          })
+
+setMethod("overlapsAny", c("hasGRanges", "GenomicRanges"),
+    IRanges:::default_overlapsAny
+)
+setMethod("overlapsAny", c("GenomicRanges", "hasGRanges"),
+    IRanges:::default_overlapsAny
+)
+setMethod("overlapsAny", c("hasGRanges", "hasGRanges"),
+    IRanges:::default_overlapsAny
+)
+
+setMethod("subsetByOverlaps", c("hasGRanges", "GenomicRanges"),
+    IRanges:::default_subsetByOverlaps
+)
+setMethod("subsetByOverlaps", c("GenomicRanges", "hasGRanges"),
+    IRanges:::default_subsetByOverlaps
+)
+setMethod("subsetByOverlaps", c("hasGRanges", "hasGRanges"),
+    IRanges:::default_subsetByOverlaps
+)
 
