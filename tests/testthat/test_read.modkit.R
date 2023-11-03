3c3e0f68848e4895db553cc378790711b68dd6d9
yixuan-chen-elisa <ychen590@jhu.edu>
2023-11-03 11:11:22 -0400

    read.modkit

---
+++ tests/testthat/test_read.modkit.R
@@ -0,0 +1,13 @@
+context("read.modkit")
+
+# TODO: Re-factor read.modkit() and update tests accordingly
+test_that("read.modkit() works for BED files", {
+    infile <- system.file("extdata", "modkit/blood.cpg.chr22.bed.gz",
+                          package = "bsseq")
+    bsseq <- read.modkit(files = infile,
+                         colData = NULL,
+                         rmZeroCov = FALSE,
+                         strandCollapse = TRUE)
+
+    lapply(bsseq, function(x) {expect_is(x, "BSseq")})
+})
