46f5ffadc9b4848a47df3b53ad88e42e5cd5aa2d
yixuan-chen-elisa <ychen590@jhu.edu>
2023-11-20 00:52:11 -0500

    update test data & news

--- tests/testthat/test_read.modbam2bed.R
+++ tests/testthat/test_read.modbam2bed.R
@@ -2,7 +2,7 @@ context("read.modbam2bed")
 
 # TODO: Re-factor read.modbam2bed() and update tests accordingly
 test_that("read.modbam2bed() works for BED files", {
-  infile <- system.file("extdata", "modbam2bed/Ctr1.bed.gz",package = "bsseq")
+  infile <- system.file("extdata", "modbam2bed/ctr1.chr10.chr11.bed.gz",package = "bsseq")
   bsseq <- read.modbam2bed(files = infile,
                         colData = NULL,
                         rmZeroCov = FALSE,
