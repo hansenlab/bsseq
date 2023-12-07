dcdd06007bdce080e0526fb2773f341476377f81
yixuan-chen-elisa <ychen590@jhu.edu>
2023-12-06 19:25:44 -0500

    update other modification

--- tests/testthat/test_read.modkit.R
+++ tests/testthat/test_read.modkit.R
@@ -2,7 +2,7 @@ context("read.modkit")
 
 # TODO: Re-factor read.modkit() and update tests accordingly
 test_that("read.modkit() works for BED files", {
-    infile <- system.file("extdata", "modkit/chr22.HG002.top1000.other_mod.bed.gz",
+    infile <- system.file("extdata", "modkit/chr21.chr22.HG002.top1000.other_mod.bed.gz",
                           package = "bsseq")
     bsseq <- read.modkit(files = infile,
                          colData = NULL,
