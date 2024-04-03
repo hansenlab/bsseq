c0c1f9ee2e0db5e9a45b6d4e1217d90bfb76180f
yixuanchen <116841407+yixuan-chen-elisa@users.noreply.github.com>
2024-04-03 16:50:25 -0400

    Update test_read.modkit.R

--- tests/testthat/test_read.modkit.R
+++ tests/testthat/test_read.modkit.R
@@ -1,13 +1,24 @@
 context("read.modkit")
 
 # TODO: Re-factor read.modkit() and update tests accordingly
-test_that("read.modkit() works for BED files", {
+test_that("read.modkit() works for BED files without 5hmc", {
     infile <- system.file("extdata", "modkit/chr21.chr22.HG002.top1000.other_mod.bed.gz",
                           package = "bsseq")
     bsseq <- read.modkit(files = infile,
                          colData = NULL,
                          rmZeroCov = FALSE,
                          strandCollapse = TRUE)
-    
+
+    lapply(bsseq, function(x) {expect_is(x, "BSseq")})
+})
+
+test_that("read.modkit() works for BED files with 5hmc", {
+    infile <- system.file("extdata", "modkit/Hypo1.first40Bed.txt",
+                          package = "bsseq")
+    bsseq <- read.modkit(files = infile,
+                         colData = NULL,
+                         rmZeroCov = FALSE,
+                         strandCollapse = TRUE)
+
     lapply(bsseq, function(x) {expect_is(x, "BSseq")})
 })
